df03a29defeadc80540b2d62b7eaf82d5ff99270
Damo <27757251@qq.com>
2019-12-13 01:16:47 +0800

    根据libAdvanceWars的变动进行了调整

--- settings.lua
+++ settings.lua
@@ -1,6 +1,9 @@
 --语言文件
 language="chinese"
 
+--全身像
+bodyImagePath="images/Commanders/Body"
+
 --数据
 FilenameCorps="datas/Corps.lua"--兵种资料
 FilenameTroops="datas/Troops.lua"--部队资料
@@ -12,17 +15,7 @@ ImagesPathCorps="images/Corps"
 ImagesPathTroops="images/Troops"
 ImagesPathTerrains="images/Terrains"
 ImagesPathCommanders="images/Commanders/Head"
+imagesPathCommandersBody="Commanders/Body";
 
-senarioScriptsPath="scripts"
---mapsPath="maps/awbw"
 mapsPath="maps/awbw"
-
---用于支持文件打开界面的操作
-defaultPath={
-	Corps={title="兵种数据",path="datas/Corps"},
-	Commanders={title="指挥官数据",path="datas/Commanders"},
-	Troops={title="部队数据",path="datas/Troops"},
-	Terrains={title="地形数据",path="datas/Terrains"},
-	Weathers={title="天气数据",path="datas/Weathers"},
-	Senarios={title="剧本数据",path="scripts"},
-}
+scenarioScriptsPath="scripts"
