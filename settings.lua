763017f6fb6e2259ca83afb3ead9dec05a3d1aea
Damo <27757251@qq.com>
2020-02-18 01:10:57 +0800

    添加了各种小图标

--- settings.lua
+++ settings.lua
@@ -10,6 +10,10 @@ dataCorps="datas/Corps.lua"--兵种资料
 dataTroops="datas/Troops.lua"--部队资料
 dataTerrainCodes="datas/Terrains.lua"--地形资料
 dataCommanders=""--指挥官资料
+dataWeathers="datas/Weathers.lua"
+
+--规则
+ruleMove="codes/movement.lua"--移动规则
 
 --纹理加载路径
 imagesPathCorps="images/Corps"
